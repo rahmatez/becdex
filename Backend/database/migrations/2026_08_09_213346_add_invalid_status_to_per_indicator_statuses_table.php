483d97f81c4059202876b29e7e595e71236cccb3
rahmatez <rahmatezdev@gmail.com>
2026-08-09 22:07:37 +0700

    chore: add invalid status migration

---
+++ Backend/database/migrations/2026_08_09_213346_add_invalid_status_to_per_indicator_statuses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('per_indicator_statuses')->insert([
+            'id' => 6,
+            'name' => 'Invalid',
+            'color' => 'danger',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('per_indicator_statuses')->where('id', 6)->delete();
+    }
+};
